d96b05818c51be3208cab18f373b7e8f58d06125
Hedwesley <hedwesleygusmao@gmail.com>
2026-04-28 14:30:35 -0300

    feat: melhorias na API para rodar no render

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,13 @@ import cors from "cors"
 import executarController from "./controllers/executarController.js"
 
 const app = express()
+const PORT = process.env.PORT || 3001;
 
 app.use(cors())
 app.use(express.json())
 
 app.post("/executar", executarController.executar)
 
-app.listen(3001, () => {
-  console.log("Servidor rodando na porta 3001")
-})
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
